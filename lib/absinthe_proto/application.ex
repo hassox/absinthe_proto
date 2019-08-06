0eca1eaf5de3260cb48f4072f6f30bec8f985ee7
Connor Jacobsen <jacobsen.connor@gmail.com>
2019-08-06 11:35:15 -0700

    refactor

--- lib/absinthe_proto/application.ex
+++ lib/absinthe_proto/application.ex
@@ -1,10 +1,21 @@
 defmodule AbsintheProto.Application do
   use Application
+  require Logger
 
   def start(_, _) do
     children = []
 
+    validate_modify_message!()
+
     opts = [strategy: :one_for_one, name: AbsintheProto.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp validate_modify_message! do
+    AbsintheProto.DSL.load_modify_message()
+  rescue
+    err ->
+      Logger.error("\n\nInvalid configuration (syntax error) for `config :absinthe_proto, :modify_message`\n\n")
+      reraise(err, __STACKTRACE__)
   end
 end
--- lib/absinthe_proto/dsl.ex
+++ lib/absinthe_proto/dsl.ex
@@ -193,12 +193,7 @@ defmodule AbsintheProto.DSL do
     {opts, _} = Module.eval_quoted(__CALLER__, options)
     ns = Keyword.get(opts, :namespace)
 
-    modify_message =
-      :absinthe_proto
-      |> Application.get_env(:modify_message, %{})
-      |> maybe_hydrate_modify_message()
-
-    build_struct = %__MODULE__{options: opts, namespace: ns, modify_message: modify_message}
+    build_struct = %__MODULE__{options: opts, namespace: ns, modify_message: load_modify_message()}
 
     save_draft_build(build_struct, __CALLER__.module)
 
@@ -230,11 +225,18 @@ defmodule AbsintheProto.DSL do
     nil
   end
 
-  defp maybe_hydrate_modify_message(mod_msg) when is_binary(mod_msg) do
+  @doc false
+  def load_modify_message do
+    :absinthe_proto
+    |> Application.get_env(:modify_message, %{})
+    |> maybe_transform_modify_message!()
+  end
+
+  defp maybe_transform_modify_message!(mod_msg) when is_binary(mod_msg) do
     {mod, _} = Code.eval_string(mod_msg)
     mod
   end
-  defp maybe_hydrate_modify_message(mod_msg), do: mod_msg
+  defp maybe_transform_modify_message!(mod_msg) when is_map(mod_msg), do: mod_msg
 
   defp apply_configured_modifiers(mod) do
     build_struct = current_draft_build!(mod)
