f6d965fc7ea27564ff534457c96b6219caa4e8de
Daniel Neighman <dneighman@gmail.com>
2019-08-05 17:22:04 -0700

    Use MF for mods instead of anon funs

--- lib/absinthe_proto/dsl.ex
+++ lib/absinthe_proto/dsl.ex
@@ -233,6 +233,8 @@ defmodule AbsintheProto.DSL do
       for mod <- mods,
                 !!Map.get(build_struct.modify_message, mod) do
         case Map.get(build_struct.modify_message, mod) do
+          {m, f} when is_atom(m) and is_atom(f) ->
+            apply(m, f, [mod])
           fun when is_function(fun) ->
             fun.(mod)
           _ -> raise "unknown modifier! expected a 1 arity function"
